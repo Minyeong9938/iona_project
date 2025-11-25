d32441693fabef9d9d3ffabf026e559b042b1ed3
Minyeong9938 <isk07179@sau.ac.kr>
2025-11-25 16:00:46 +0900

    배경 핀 효과

--- assets/js/pages/sub-tech.js
+++ assets/js/pages/sub-tech.js
@@ -1,71 +1,58 @@
 export default function initTechPage() {
-    const videoSection = document.querySelector(".video-sec");
-    const text1 = document.querySelector(".text01");
-    const text2 = document.querySelector(".text02");
-
-    if (!videoSection || !text1 || !text2) return;
-
-    text1.style.opacity = '1';
-    text2.style.opacity = '0';
-
-    text2.style.transform = 'translate(-50%, 70%)';
-
-    function onScroll() {
-        const secRect = videoSection.getBoundingClientRect();
-        const viewportHeight = window.innerHeight;
+    // device section
+    const swiper = new Swiper(".mySwiper", {
+        slidesPerView: 5,
+        spaceBetween: 30,
+        loop: true,
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+    });
 
 
-        const t1StartPoint = 0;
-        const t1EndPoint = -viewportHeight * 0.7;
-        let t1Progress = Math.min(Math.max((t1StartPoint - secRect.top) / (t1StartPoint - t1EndPoint), 0), 1);
 
+    // tech-about section
+    gsap.registerPlugin(ScrollTrigger);
 
-        if (secRect.top >= t1StartPoint) {
+    const ani7 = gsap.timeline();
 
-            text1.style.top = `${secRect.top + viewportHeight / 2}px`;
-            text1.style.transform = `translate(-50%, -50%)`;
-            text1.style.opacity = '1';
+    const delay = "+=3";
+    const moveDist = 200;
 
-        } else if (secRect.top > t1EndPoint) {
-            text1.style.top = `${viewportHeight / 2}px`;
-            text1.style.transform = `translate(-50%, ${-50 - 60 * t1Progress}%)`;
+    const animDuration = 2;
+    const animEase = "power2.inOut";
 
-            const fadeOutStart = 0.1;
-            const fadeOutEnd = 0.7;
-            const fadeOutRange = fadeOutEnd - fadeOutStart;
+    ani7.from(".tech-about .t1", {
+        autoAlpha: 0,
+        duration: animDuration,
+        y: moveDist,
+        ease: animEase
+    }, "start")
 
-            let fadeOutProgress = Math.min(Math.max((t1Progress - fadeOutStart) / fadeOutRange, 0), 1);
+        .to("#tech-about .t1", {
+            autoAlpha: 0,
+            duration: animDuration,
+            y: -moveDist,
+            ease: animEase
+        }, delay)
+        .from("#tech-about .t2", {
+            autoAlpha: 0,
+            duration: animDuration,
+            y: moveDist,
+            ease: animEase
+        }, "<")
 
-            text1.style.opacity = `${1 - fadeOutProgress}`;
+        .to("#tech-about .t2", { autoAlpha: 0, duration: animDuration, y: -moveDist, ease: animEase }, delay);
 
-        } else {
-            text1.style.opacity = '0';
-        }
-
-        const t2StartPoint = viewportHeight * 0.2;
-        const t2EndPoint = viewportHeight * 1.5;
-
-        let t2Progress = Math.min(Math.max((secRect.top + t2StartPoint) / (t2StartPoint - t2EndPoint), 0), 1);
-
-        const fadeInStart = 0.1;
-        const fadeInEnd = 0.7;
-        const fadeInRange = fadeInEnd - fadeInStart;
-
-        let fadeInProgress = Math.min(Math.max((t2Progress - fadeInStart) / fadeInRange, 0), 1);
-
-        text2.style.opacity = `${fadeInProgress}`;
-
-        text2.style.transform = `translate(-50%, ${70 - 80 * t2Progress}%)`;
-
-        if (secRect.top <= -(viewportHeight * 1.5)) {
-            videoSection.style.position = 'relative';
-        } else {
-            videoSection.style.position = 'sticky';
-            videoSection.style.top = '0';
-        }
-    }
-
-
-    window.addEventListener("scroll", onScroll);
-    onScroll();
+    ScrollTrigger.create({
+        animation: ani7,
+        trigger: "#tech-about",
+        start: "top top",
+        end: "+=4000",
+        scrub: true,
+        pin: true,
+        anticipatePin: 1,
+        // markers: true
+    });
 }
