e4b71837f71e9c44e6a02898168dc1cfa939de03
Minyeong9938 <isk07179@sau.ac.kr>
2025-11-25 17:48:09 +0900

    뷰티테크 페이지 디바이스 테크부분 완성해야함

--- assets/js/pages/sub-tech.js
+++ assets/js/pages/sub-tech.js
@@ -15,15 +15,15 @@ export default function initTechPage() {
     // tech-about section
     gsap.registerPlugin(ScrollTrigger);
 
-    const ani7 = gsap.timeline();
+    const ani = gsap.timeline();
 
     const delay = "+=3";
-    const moveDist = 200;
+    const moveDist = 300;
 
     const animDuration = 2;
     const animEase = "power2.inOut";
 
-    ani7.from(".tech-about .t1", {
+    ani.from("#tech-about .t1", {
         autoAlpha: 0,
         duration: animDuration,
         y: moveDist,
@@ -43,11 +43,65 @@ export default function initTechPage() {
             ease: animEase
         }, "<")
 
-        .to("#tech-about .t2", { autoAlpha: 0, duration: animDuration, y: -moveDist, ease: animEase }, delay);
+        .to("#tech-about .t2", { autoAlpha: 1, duration: animDuration, y: -moveDist, ease: animEase }, delay);
 
     ScrollTrigger.create({
-        animation: ani7,
+        animation: ani,
         trigger: "#tech-about",
+        start: "top top",
+        end: "+=3000",
+        scrub: true,
+        pin: true,
+        anticipatePin: 1,
+        // markers: true
+    });
+
+    // research section
+    const aniResearch = gsap.timeline();
+
+    aniResearch.from("#research-about .r1", {
+        autoAlpha: 0,
+        duration: animDuration,
+        y: moveDist,
+        ease: animEase
+    }, "start")
+
+        .to("#research-about .r1", {
+            autoAlpha: 0,
+            duration: animDuration,
+            y: -moveDist,
+            ease: animEase
+        }, delay)
+        .from("#research-about .r2", {
+            autoAlpha: 0,
+            duration: animDuration,
+            y: moveDist,
+            ease: animEase
+        }, "< + 0.1")
+
+        .to("#research-about .r2", {
+            autoAlpha: 0,
+            duration: animDuration,
+            y: -moveDist,
+            ease: animEase
+        }, "+=3")
+        .from("#research-about .r3", {
+            autoAlpha: 0,
+            duration: animDuration,
+            y: moveDist,
+            ease: animEase
+        }, "< + 0.1")
+
+        .to("#research-about .r3", {
+            autoAlpha: 1,
+            duration: animDuration,
+            y: -moveDist,
+            ease: animEase
+        }, "+=3");
+
+    ScrollTrigger.create({
+        animation: aniResearch,
+        trigger: "#research-about",
         start: "top top",
         end: "+=4000",
         scrub: true,
