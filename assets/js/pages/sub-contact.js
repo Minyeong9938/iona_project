efea0b9acac96f9510b1373dc68bf23d47f15612
Minyeong9938 <isk07179@sau.ac.kr>
2025-11-27 18:51:34 +0900

    문의

--- assets/js/pages/sub-contact.js
+++ assets/js/pages/sub-contact.js
@@ -1,3 +1,17 @@
 export default function initContactPage() {
+    var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
+        mapOption = {
+            center: new kakao.maps.LatLng(37.447545, 126.702755),
+            level: 3
+        };
 
+    var map = new kakao.maps.Map(mapContainer, mapOption);
+
+    var markerPosition = new kakao.maps.LatLng(37.447545, 126.702755);
+
+    var marker = new kakao.maps.Marker({
+        position: markerPosition
+    });
+
+    marker.setMap(map);
 }
