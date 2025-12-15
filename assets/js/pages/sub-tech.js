01bc791b20c6d36b7596a31041d6df1c7ac6f058
Minyeong9938 <isk07179@sau.ac.kr>
2025-12-15 14:59:22 +0900

    mo

--- assets/js/pages/sub-tech.js
+++ assets/js/pages/sub-tech.js
@@ -49,24 +49,29 @@ techAboutContents.forEach((el, index) => {
 });
 
 // device-tech section
-let horizontalItems = gsap.utils.toArray('.device-tech-sec .device-bg');
+gsap.matchMedia().add("(min-width: 1025px)", () => {
+    // 1025px 이상일 때만 가로 스크롤 애니메이션 실행
+    let horizontalItems = gsap.utils.toArray('.device-tech-sec .device-bg');
 
-// 슬라이드 개수
-const numItems = horizontalItems.length;
+    // 슬라이드 개수
+    const numItems = horizontalItems.length;
 
-// .horizontal-wrap 전체 너비를 (슬라이드 개수 * 100vw)로 설정했으므로, 
-// 마지막 슬라이드가 뷰포트 끝에 올 때까지 스크롤 되도록 xPercent를 계산합니다.
-gsap.to(horizontalItems, {
-    xPercent: -100 * (numItems - 1), // 예: 4개일 경우 -300%
-    ease: 'none',
-    scrollTrigger: {
-        trigger: '.device-tech-sec',
-        pin: true, // 섹션을 화면에 고정
-        scrub: 3, // 스크롤에 따라 부드럽게 애니메이션
-        // end: "+=4000", // 스크롤 길이 설정 (4000px 동안 애니메이션 진행)
-        end: () => `+=${numItems * 1000}`, // 슬라이드 개수에 비례하여 스크롤 길이 설정 (선택 사항)
-        // markers: true // 디버깅용 마커 (필요 없으면 주석 처리)
-    }
+    gsap.to(horizontalItems, {
+        xPercent: -100 * (numItems - 1),
+        ease: 'none',
+        scrollTrigger: {
+            trigger: '.device-tech-sec',
+            pin: true, // 섹션을 화면에 고정
+            scrub: 1.5, // 스크롤에 따라 부드럽게 애니메이션
+            end: () => `+=${numItems * 1000}`,
+        }
+    });
+
+    // MatchMedia 블록이 끝날 때 애니메이션을 정리할 수 있도록 return
+    return () => {
+        // 클린업 로직 (필요시)
+        ScrollTrigger.getById('device-tech-scroll').kill();
+    };
 });
 
 // research section
