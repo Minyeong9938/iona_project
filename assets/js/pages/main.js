4eb639d2f109ba02fe90254d94ef602c3db3f699
Minyeong9938 <isk07179@sau.ac.kr>
2025-12-16 18:17:34 +0900

    수정

--- assets/js/pages/main.js
+++ assets/js/pages/main.js
@@ -103,24 +103,18 @@ if (techSec && cursor) {
 
     const swiper = new Swiper('.mySwiper', {
         slidesPerView: 'auto',
-        spaceBetween: 20,
+        spaceBetween: 24,
         watchOverflow: true,
         centeredSlides: false,
         allowTouchMove: true,
 
         breakpoints: {
-            480: {
-                spaceBetween: 60,
-            },
-
             640: {
                 slidesPerView: 3,
-                spaceBetween: 20,
             },
 
             1024: {
                 slidesPerView: 4,
-                spaceBetween: 30,
             },
         },
 
